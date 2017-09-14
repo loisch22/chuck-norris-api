b85cfdf58cb790cac8144b1f1d3468fd204522c0
Lois Choi <loisch22@gmail.com>
2017-09-13 17:30:44 -0700

    move es6 promise from user to business

--- js/chuck_norris.js
+++ js/chuck_norris.js
@@ -8,6 +8,27 @@ Joke.prototype.getJoke = function() {
   }).fail(function(error) {
     $('.errors').text('There was an error while processing your joke. Please try again.');
   });
+
+  // let promise = new Promise(function(resolve, reject) {
+  //   let request = new XMLHttpRequest();
+  //   let url = `https://api.chucknorris.io/jokes/random`;
+  //   request.onload = function() {
+  //     if (this.status === 200) {
+  //       resolve(request.response);
+  //     } else {
+  //       reject();
+  //     }
+  //   }
+  //   request.open("GET", url, true);
+  //   request.send();
+  // });
+  //
+  // promise.then(function(response) {
+  //   let body = JSON.parse(response);
+  //   $('.showJokes').text(`${body.value}`);
+  // }, function(error) {
+  //   $('.errors').text('Oh no. Something went wrong! Please try again.');
+  // });
 }
 
 exports.jokeModule = Joke;
